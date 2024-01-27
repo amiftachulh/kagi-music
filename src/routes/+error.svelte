b38e1a7809bf95d2392500b74c7de89675438097
Ahmad Miftachul Hidayat <miftachul.mkb@gmail.com>
2024-01-27 16:59:07 +0700

    feat: add custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+</script>
+
+<main>
+  <h1>{$page.status}: {$page.error?.message}</h1>
+</main>
+
+<style>
+  main {
+    min-height: calc(100dvh - 10rem);
+  }
+</style>
